28f7eb44ecd80b9287a42600b4e78a65428a497d
xavyan <xavieryanquin@gmail.com>
2021-03-14 14:08:02 +0000

    Removed commented code in main.cc

--- src/main.cc
+++ src/main.cc
@@ -9,30 +9,4 @@ int main (int argc, char* argv[]) {
 	Game game(1, 1, kb);
 	game.start();
 	std::cout << "Finished!\n";
-
-	// KeyboardController controller;
-
-	// int last_view = RIGHT;
-
-	// while (true) {
-	// 	int c = controller.get_snake_view();
-	// 	if (c != last_view) {
-	// 		std::cout << "Se ha detectado un cambio! Sentido actual: ";
-	// 		switch (c) {
-	// 			case UP:
-	// 				std::cout << "UP";
-	// 				break;
-	// 			case RIGHT:
-	// 				std::cout << "RIGHT";
-	// 				break;
-	// 			case DOWN:
-	// 				std::cout << "DOWN";
-	// 				break;
-	// 			case LEFT:
-	// 				std::cout << "LEFT";
-	// 		}
-	// 		std::cout << '\n';
-	// 		last_view = c;
-	// 	}
-	// }
 }
